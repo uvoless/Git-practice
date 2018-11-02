5a5bd9e3820b522ca32426a32d5d50f5b4d6d455
juiyingchiu <uvoless@gmail.com>
2018-11-02 09:47:23 +0800

    Initial Commit

---
+++ Git practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Git practice
+//
+//  Created by juiying chiu on 2018/11/2.
+//  Copyright © 2018 juiying chiu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
